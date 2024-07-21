4fa0599374fbd8b1041f06ad1d8c883ac4efa775
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-07-21 21:39:56 +0530

    merge k sorted lists

--- Linked Lists/merge_k_sorted_lists.cpp
+++ Linked Lists/merge_k_sorted_lists.cpp
@@ -13,16 +13,15 @@ typedef long long ll;
 #define ss     second 
 #define endl  "\n"
 using namespace std;
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
 
