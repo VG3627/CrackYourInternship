d522e65387ceb3182ee96f2cc1fda7ea325d92ff
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-07-27 11:50:32 +0530

    reverse linked list

---
+++ Linked Lists/reverse_linked_list.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vi     vector <int>
+#define vll    vector <long long>
+#define vs     vector <string>
+#define vc     vector <char>
+#define PII    pair<int,int>
+#define mll    map<long long,long long>
+#define UMII   unordered_map<int,int>
+#define sz(a)  a.size()
+#define all(a) a.begin(),a.end()
+#define ff     first 
+#define ss     second 
+#define endl  "\n"
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        ListNode *prev = NULL ;
+        while(head != NULL)
+        {
+           ListNode *temp = head->next ;
+           head->next = prev ;
+           prev = head ;
+           head = temp ;
+        }
+
+        return prev ;
+    }
+};
+void solve()
+{
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t ;
+    cin>>t ;
+    while(t--)
+    {
+        solve() ;
+    }   
+    return 0;
+}
