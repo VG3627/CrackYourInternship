4ea4b4aaeabb86ee00ad49935e4631c7fd279dff
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-07-27 16:28:31 +0530

    sort list

---
+++ Linked Lists/sort_list.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vi     vector <int>
+#define vll    vector <long long>
+#define vs     vector <string>
+#define vc     vector <char>
+#define PII    pair<int,int>
+#define mll    map<long long,long long>
+#define UMII   unordered_map<int,int>
+#define sz(a)  a.size()
+#define all(a) a.begin(),a.end()
+#define ff     first 
+#define ss     second 
+#define endl  "\n"
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int>a ;
+        ListNode* x = head ;
+        while(x != NULL)
+        {
+            a.push_back(x->val) ;
+            x = x->next ;
+        }
+        sort(a.begin(),a.end()) ;
+        
+        ListNode* y = head ;
+        int  i = 0 ;
+        while(y != NULL)
+        {
+            y->val = a[i] ;
+            i++ ;
+            y = y->next ;
+        } 
+        return head ;
+        
+    }
+};
+void solve()
+{
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t ;
+    cin>>t ;
+    while(t--)
+    {
+        solve() ;
+    }   
+    return 0;
+}
