a6e2d844fe67d1747021104f330434c2c6854849
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-07-31 16:45:10 +0530

    remove duplicates from sorted array

---
+++ arrays/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vi     vector <int>
+#define vll    vector <long long>
+#define vs     vector <string>
+#define vc     vector <char>
+#define PII    pair<int,int>
+#define mll    map<long long,long long>
+#define UMII   unordered_map<int,int>
+#define sz(a)  a.size()
+#define all(a) a.begin(),a.end()
+#define ff     first 
+#define ss     second 
+#define endl  "\n"
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& a) {
+        
+
+        int i=0;
+        for(int j=1;j<a.size();j++)
+        {
+            if(a[i]!=a[j])
+            {
+                i++;
+                a[i]=a[j];
+            }
+        }
+        return i+1;
+    }
+};
+void solve()
+{
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t ;
+    cin>>t ;
+    while(t--)
+    {
+        solve() ;
+    }   
+    return 0;
+}
