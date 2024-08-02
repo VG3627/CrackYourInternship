dc739cd32bdb36783f3827762582398edb79241c
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-08-02 10:15:49 +0530

    move zeroes

---
+++ arrays/move_zeroes.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vi     vector <int>
+#define vll    vector <long long>
+#define vs     vector <string>
+#define vc     vector <char>
+#define PII    pair<int,int>
+#define mll    map<long long,long long>
+#define UMII   unordered_map<int,int>
+#define sz(a)  a.size()
+#define all(a) a.begin(),a.end()
+#define ff     first 
+#define ss     second 
+#define endl  "\n"
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.size();i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
+        }
+    }
+};
+     
+void solve()
+{
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t ;
+    cin>>t ;
+    while(t--)
+    {
+        solve() ;
+    }   
+    return 0;
+}
