559acdf503303d6da2ffe3378e1d58706bc0f5e4
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-08-03 23:56:49 +0530

    excel sheet column title

---
+++ mathematical/excel_sheet_column_title.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vi     vector <int>
+#define vll    vector <long long>
+#define vs     vector <string>
+#define vc     vector <char>
+#define PII    pair<int,int>
+#define mll    map<long long,long long>
+#define UMII   unordered_map<int,int>
+#define sz(a)  a.size()
+#define all(a) a.begin(),a.end()
+#define ff     first 
+#define ss     second 
+#define endl  "\n"
+using namespace std;
+class Solution {
+public:
+    string convertToTitle(int n) {
+        
+        string ans ;
+        while(n)
+        {
+            n-- ;
+            int x = n % 26 ;
+            ans.push_back(char('A' + x)) ;
+            n /= 26 ;
+        }
+        reverse(ans.begin(),ans.end()) ;
+        return ans ;
+     
+    }
+};
+void solve()
+{
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t ;
+    cin>>t ;
+    while(t--)
+    {
+        solve() ;
+    }   
+    return 0;
+}
